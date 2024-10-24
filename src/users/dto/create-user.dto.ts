6f495ae47c7fedb8a662ade5e6f3a9f9d72f1e8f
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-24 09:43:05 -0300

    feat: create Users dto's, entity and repository

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  admin: boolean;
+}

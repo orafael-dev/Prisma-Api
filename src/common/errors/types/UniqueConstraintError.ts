0f2ca165128db3c79ee0427a68232436e51a3d4e
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-25 08:07:01 -0300

    feat: create Posts Module, Service, Controller, and Create/update Dto

---
+++ src/common/errors/types/UniqueConstraintError.ts
@@ -0,0 +1,9 @@
+import { ConflictError } from './ConflictError';
+import { PrismaClientError } from './PrismaClientError';
+
+export class UniqueConstraintError extends ConflictError {
+  constructor(e: PrismaClientError) {
+    const uniqueField = e.meta.target[0];
+    super(`The ${uniqueField} is already taken`);
+  }
+}
---
+++ src/common/errors/utils/handle-database-errors.util.ts
@@ -0,0 +1,16 @@
+import { DatabaseError } from '../types/DatabaseError';
+import { PrismaClientError } from '../types/PrismaClientError';
+import { UniqueConstraintError } from '../types/UniqueConstraintError';
+
+enum PrismaErrors {
+  UniqueConstraintfail = 'P2002',
+}
+
+export const handleDatabaseErrors = (e: PrismaClientError): Error => {
+  switch (e.code) {
+    case PrismaErrors.UniqueConstraintfail:
+      return new UniqueConstraintError(e);
+    default:
+      return new DatabaseError(e.message);
+  }
+};
